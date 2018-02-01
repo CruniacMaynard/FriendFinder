8378bc1b77c7bc4ea1ea0ce6f42dad0277c49c95
Cruniac <s.maynard21@yahoo.com>
2018-02-01 17:28:19 -0500

    files

---
+++ app/data/friends.js
@@ -0,0 +1,6 @@
+
+// Variable to hold the array of friends objects data
+var friends = [{ "name": "Kevin Love", "photo": "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/3449.png&w=350&h=254", "scores": ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"] }, { "name": "LeBron James", "photo": "https://cdn-s3.si.com/s3fs-public/teams/basketball/nba/players/214152-300x300.png", "scores": ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"] }, { "name": "Kyle Korver", "photo": "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/2011.png&w=350&h=254", "scores": ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"] }, { "name": "Jeff Green", "photo": "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/3209.png&w=350&h=254", "scores": ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"] }, { "name": "JR Smith", "photo": "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/2444.png&w=350&h=254", "scores": ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"] }, { "name": "Channing Frye", "photo": "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/2754.png&w=350&h=254", "scores": ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"] }];
+
+// Export the data
+module.exports = friends;
